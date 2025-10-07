ba89b4fd0f9b7f61cd7f1294d9b1ee835e163a01
bodeal5ofa4 <bolaraafat2003@gmail.com>
2025-10-07 16:29:28 +0300

    Fix mobile projects section

--- lib/feature/home/ui/mobile_layout/sections/contact_section_mobile/contact_section_mobile.dart
+++ lib/feature/home/ui/mobile_layout/sections/contact_section_mobile/contact_section_mobile.dart
@@ -12,42 +12,45 @@ class ContactSectionMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(children: [
-          SectionName(firstnane: 'Contact', secondname: 'Me',divider: 0.4,),
-          SizedBox(height: 20,),
-          Container(
-        padding: EdgeInsets.all(30),
-        decoration: BoxDecoration(color: AppColors.lightBackgroundColor,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: AppColors.primaryColor,
-        width: 1.5,)
-        
-        ,),
-            child: Column(
-              children: [
-                 Text(
-                        'Hi, I’m Bola Raafat 👋\nA passionate Flutter Developer who loves creating mobile apps with clean design and great user experience.\nI’m always open to new opportunities, collaborations, or just a good tech chat.',
-                      style: AppStyles.bold20.copyWith(color: Colors.white,fontSize: 16),),
-                      SizedBox(height: 16,),
-                      Align( alignment: Alignment.centerLeft ,child: Text("Let's Work Together",style: AppStyles.bold20.copyWith(color: Colors.white,fontSize: 16),)),
-                      SizedBox(height: 16,),
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      //  crossAxisAlignment: CrossAxisAlignment.,
-                        spacing: 8,
-                        children: [
-                        ContactIcon(icon: Icons.phone, link: 'tel:01277296970'),
-                        ContactIcon(icon: Icons.wechat, link: 'https://wa.me/201277296970'),
-                        ContactIcon(icon: Icons.email, link: 'mailto:bolaraafat2003@gmail.com?subject=Hello&body=I%20want%20to%20connect%20with%20you'),
-                       // ContactIcon(icon: Icons.location_on, link: ''),
-                        ContactIcon(icon: Icons.facebook, link: 'https://www.facebook.com/bola.rafaat.31/'),
-                            
-                      ],),
-                      SizedBox(height: 16,),
-                      SendMessageWidget()
-              ],
-            ),),
-        ],),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [
+            SectionName(firstnane: 'Contact', secondname: 'Me',divider: 0.4,),
+            SizedBox(height: 20,),
+            Container(
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(color: AppColors.lightBackgroundColor,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: AppColors.primaryColor,
+          width: 1.5,)
+          
+          ,),
+              child: Column(
+                children: [
+                   Text(
+                          'Hi, I’m Bola Raafat 👋\nA passionate Flutter Developer who loves creating mobile apps with clean design and great user experience.\nI’m always open to new opportunities, collaborations, or just a good tech chat.',
+                        style: AppStyles.bold20.copyWith(color: Colors.white,fontSize: 16),),
+                        SizedBox(height: 16,),
+                        Align( alignment: Alignment.centerLeft ,child: Text("Let's Work Together",style: AppStyles.bold20.copyWith(color: Colors.white,fontSize: 16),)),
+                        SizedBox(height: 16,),
+                        Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        //  crossAxisAlignment: CrossAxisAlignment.,
+                          spacing: 8,
+                          children: [
+                          ContactIcon(icon: Icons.phone, link: 'tel:01277296970'),
+                          ContactIcon(icon: Icons.wechat, link: 'https://wa.me/201277296970'),
+                          ContactIcon(icon: Icons.email, link: 'mailto:bolaraafat2003@gmail.com?subject=Hello&body=I%20want%20to%20connect%20with%20you'),
+                         // ContactIcon(icon: Icons.location_on, link: ''),
+                          ContactIcon(icon: Icons.facebook, link: 'https://www.facebook.com/bola.rafaat.31/'),
+                              
+                        ],),
+                        SizedBox(height: 16,),
+                        SendMessageWidget()
+                ],
+              ),),
+          ],),
+      ),
     );
   }
 }
